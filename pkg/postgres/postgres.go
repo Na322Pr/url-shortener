b62372fcd20b91ff37054cf11c90604d4062f2c4
Alexander Marchenko <alex@MacBook-Air-Alexander.local>
2024-07-11 10:13:26 +0300

    Add migrator

--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -24,17 +24,6 @@ func Connection(url string) (*Postgres, error) {
 		return nil, fmt.Errorf("error database connecting: %w", err)
 	}
 
-	_, err = pg.Conn.Exec(context.Background(),
-		`CREATE TABLE IF NOT EXISTS urls (
-		id SERIAL PRIMARY KEY,
-		url TEXT NOT NULL,
-		alias TEXT NOT NULL UNIQUE);
-		CREATE INDEX IF NOT EXISTS idx_alias ON urls(alias);`)
-
-	if err != nil {
-		return nil, fmt.Errorf("error in database: %w", err)
-	}
-
 	return pg, nil
 }
 
